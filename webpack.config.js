a1e111bc1c7360911facb8b86b346af5da64c808
DenisovaSS <114432399+DenisovaSS@users.noreply.github.com>
2024-04-02 17:34:52 +0300

    download all setting

---
+++ webpack.config.js
@@ -0,0 +1,80 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const PostcssPresetEnv = require('postcss-preset-env');
+
+const mode = process.env.NODE_ENV || 'development';
+const devMode = mode === 'development';
+const target = devMode ? 'web' : 'browserslist';
+const devtool = devMode ? 'source-map' : undefined;
+module.exports = {
+  mode,
+  target,
+  devtool,
+  devServer: {
+    open: true,
+  },
+  entry: path.resolve(__dirname, 'src', 'index.ts'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+    filename: 'main.js',
+    assetModuleFilename: 'assets/[name].[ext]',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'src', 'index.html'),
+    }),
+    new MiniCssExtractPlugin({ filename: '[name].[contenthash].css' }),
+  ],
+  resolve: {
+    // Add `.ts` and `.tsx` as a resolvable extension.
+    extensions: ['.ts', '.tsx', '.js'],
+    // Add support for TypeScripts fully qualified ESM imports.
+    extensionAlias: {
+      '.js': ['.js', '.ts'],
+      '.cjs': ['.cjs', '.cts'],
+      '.mjs': ['.mjs', '.mts'],
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.html$/i,
+        loader: 'html-loader',
+      },
+      {
+        test: /\.([cm]?ts|tsx)$/,
+        loader: 'ts-loader',
+        exclude: /node_modules/,
+      },
+
+      {
+        test: /\.(c|sa|sc)ss$/i,
+        use: [
+          devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
+          'css-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: [PostcssPresetEnv],
+              },
+            },
+          },
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+        generator: { filename: 'fonts/[name].[ext]' },
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+        generator: { filename: 'img/[name].[ext]' },
+      },
+    ],
+  },
+};
